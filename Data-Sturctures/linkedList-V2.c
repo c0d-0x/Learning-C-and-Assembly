07d3190e7dcdb6ddb3fc4ec85e9cb516a81f8dca
c0d-0x <c0d_0x007@proton.me>
2023-11-03 21:32:01 +0100

    Refreshing on linkedlists

---
+++ Data-Sturctures/linkedList-V2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* This was implememted for more understanding of memeory safety with malloc. */
+typedef struct Node {
+    int value;
+    struct Node * next;
+} Node ;
+
+void insert_end(Node ** root, int value){
+    Node * new_node = malloc(sizeof(Node));
+    if (new_node == NULL){
+        exit(1);
+    }
+    new_node->value = value;
+    if (*root == NULL) {
+
+        new_node->next = *root;
+        *root = new_node;
+        return;
+    }
+    Node * tmp = *root;
+    while (tmp->next != NULL ) {
+        tmp = tmp->next;
+    }
+    new_node->next = NULL;
+    tmp->next = new_node;
+
+}
+
+void printlist(Node * head){
+    Node * tmp = head;
+    while (tmp != NULL) {
+        printf("%d\n", tmp->value);
+        tmp = tmp->next;
+    }
+}
+
+int main(){
+    Node * head;
+    head = NULL;
+    insert_end(&head, 3);
+    insert_end(&head, 4);
+    
+    printlist(head);
+
+
+}
