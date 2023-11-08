7a9a102686a46ab779eb58d6cb3d7167386c8760
c0d-0x <c0d_0x007@proton.me>
2023-11-08 22:05:36 +0100

    Node deletion

--- Data-Sturctures/linkedList-V2.c
+++ Data-Sturctures/linkedList-V2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /* This was implememted for more understanding of memeory safety with malloc. */
+
 typedef struct Node {
     int value;
     struct Node * next;
@@ -34,6 +36,25 @@ void insert_head(Node ** head, int value){
     new_node->next = *head;
     *head = new_node;
 }
+
+void deleNode(Node ** head, int value){
+    //this function deletes all the occurance of a Node by value.
+    Node *tmp = *head;
+
+    if ((*head)->value == value) {
+       *head = (*head)->next;
+       free(tmp);
+    }
+    Node *prev = tmp;
+    do {
+        if(tmp->value == value){
+            
+        }
+    
+    } while(tmp->next != NULL);
+}
+
+
 void printlist(Node * head){
     Node * tmp = head;
     while (tmp != NULL) {
@@ -47,10 +68,9 @@ int main(){
     head = NULL;
     insert_end(&head, 3);
     insert_end(&head, 4);
-   insert_head(&head, 2); 
-   insert_head(&head, 1); 
-   insert_head(&head, 0); 
+    insert_head(&head, 2); 
+    insert_head(&head, 1); 
+    insert_head(&head, 0); 
     printlist(head);
-
 
 }
