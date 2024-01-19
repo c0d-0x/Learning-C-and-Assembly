22d9a258973f836ce9bdc0760918f6922613be1e
c0d-0x <c0d_0x007@proton.me>
2024-01-19 17:31:19 +0100

    Random password generator function implimentation

--- Password-manager/main.c
+++ Password-manager/main.c
@@ -1,3 +1,4 @@
+#include "password.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,14 +13,20 @@ void help() {
          "all saved "
          "passwords\n");
 }
+
 int main(int argc, char *argv[]) {
+
   if (argc < 2) {
     help();
     return 1;
   }
+
   if (strncmp(argv[1], "-h", sizeof(char) * 2) == 0) {
+
     help();
+
   } else if (strncmp(argv[1], "-g", sizeof(char) * 2) == 0) {
+
     if (argc != 4) {
       fprintf(stderr, " usage: %s <-g> <password> <account or username>",
               argv[0]);
@@ -29,12 +36,17 @@ int main(int argc, char *argv[]) {
     // call password generator
     // Authenication
     // writing to the Database
+
   } else if (strncmp(argv[1], "-l", sizeof(char) * 2) == 0) {
+
     // Authenication
     // list all passwords stored in the Database.
+
   } else if (strncmp(argv[1], "-r", sizeof(char) * 2) == 0) {
-    // call random password generator
-    //
+
+    void *password = random_password();
+    printf("%s\n", (char *)password);
+    free(password);
   } else if (strncmp(argv[1], "-s", sizeof(char) * 2) == 0) {
     if (argc != 3) {
       fprintf(stderr, " usage: %s <-s> <account or username>", argv[1]);
