c61ece94e4e2ddca2698fa748598e7fd1903501d
c0d-0x <c0d_0x007@proton.me>
2024-01-24 04:39:09 +0100

    migrated password manager project

--- Password-manager/password.h
+++
@@ -1,5 +0,0 @@
-void *random_password(void);
-void save_password(const char *password,
-                   const char *account); // takes in random_password as argument
-                                         // then saves in a database.
-void *authentication(void *); // takes in a an address of the master password.
