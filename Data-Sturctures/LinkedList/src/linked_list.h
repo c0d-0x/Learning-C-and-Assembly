d1671481b69fc9d21646d61db6dcfa0d4c4f4a00
c0d-0x <c0d_0x007@proton.me>
2024-05-28 23:50:28 +0100

    Linked_list_lib

---
+++ Data-Sturctures/LinkedList/src/linked_list.h
@@ -0,0 +1,20 @@
+#ifndef LINKED_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+  void *data;
+  struct node *next;
+} node_t;
+
+node_t *init_list(void);
+void print_list(node_t *head);
+int insert_to_tale(node_t **head, void *data);
+int insert_to_head(node_t **head, void *data);
+void *delete_by_position(node_t **head, size_t position);
+void *delete_by_data(node_t **head, void *data);
+node_t *pop_head(node_t **head);
+node_t *break_tale(node_t **head);
+void distroy_list(node_t **head);
+#endif // !LINKED_LIST_H
